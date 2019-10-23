c1201b97296117a1a64e98c36754365b94238a9f
Shubham Sachdeva <ss1594@g.rit.edu>
2019-10-23 16:16:41 -0400

    added bouncy platforms

---
+++ MinerInconvenience/Assets/Scripts/JellyFier.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JellyFier : MonoBehaviour
+{
+    public float bounceSpeed;
+    public float fallForce;
+    public float stiffness;
+
+    private MeshFilter meshFilter;
+    private Mesh mesh;
+
+    JellyVertex[] jellyVertices;
+    Vector3[] currentMeshVertices;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        meshFilter = GetComponent<MeshFilter>();
+        mesh = meshFilter.mesh;
+
+        GetVertices();
+    }
+
+    void GetVertices()
+    {
+        jellyVertices = new JellyVertex[mesh.vertices.Length];
+        currentMeshVertices = new Vector3[mesh.vertices.Length];
+
+        for (int i = 0; i < mesh.vertices.Length; i++)
+        {
+            jellyVertices[i] = new JellyVertex(i, mesh.vertices[i], mesh.vertices[i], Vector3.zero);
+            currentMeshVertices[i] = mesh.vertices[i];
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateVertices();
+    }
+
+    void UpdateVertices()
+    {
+        for (int i = 0; i < jellyVertices.Length; i++)
+        {
+            jellyVertices[i].UpdateVelocity(bounceSpeed);
+            jellyVertices[i].Settle(stiffness);
+
+            jellyVertices[i].currentVertexPosition += jellyVertices[i].currentVelocity * Time.deltaTime;
+            currentMeshVertices[i] = jellyVertices[i].currentVertexPosition;
+        }
+
+        mesh.vertices = currentMeshVertices;
+        mesh.RecalculateBounds();
+        mesh.RecalculateNormals();
+        mesh.RecalculateTangents();
+    }
+
+    public void OnCollisionStay(Collision collision)
+    {
+        if (collision.gameObject.transform.position.y > transform.position.y)
+        {
+            ContactPoint[] collisionPoints = collision.contacts;
+            Vector2 offset = GetComponentInChildren<CapsuleCollider2D>().offset;
+            offset.y = -0.3f;
+            GetComponentInChildren<CapsuleCollider2D>().offset = offset;
+
+            Vector3 curOffset = GetComponent<BoxCollider>().center;
+            curOffset.y = -0.1f;
+            //GetComponent<BoxCollider>().center=curOffset;
+
+            for (int i = 0; i < collisionPoints.Length; i++)
+            {
+                Vector3 inputPoint = collisionPoints[i].point + (collisionPoints[i].point * .05f);
+                ApplyPressureToPoint(inputPoint, fallForce);
+            }
+        }
+    }
+
+    public void OnCollisionExit(Collision collision)
+    {
+        Vector2 offset = GetComponentInChildren<CapsuleCollider2D>().offset;
+        offset.y = 0f;
+        GetComponentInChildren<CapsuleCollider2D>().offset = offset;
+
+        Vector3 curOffset = GetComponent<BoxCollider>().center;
+        curOffset.y = 0f;
+        GetComponent<BoxCollider>().center = curOffset;
+    }
+
+    public void ApplyPressureToPoint(Vector3 point, float pressure)
+    {
+        for (int i = 0; i < jellyVertices.Length; i++)
+        {
+            jellyVertices[i].ApplyPressureToVertex(transform, point, pressure);
+        }
+    }
+}
---
+++ MinerInconvenience/Assets/Scripts/JellyVertex.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JellyVertex
+{
+    //fields
+    public int vertexIndex;
+    public Vector3 intialVertexPosition;
+    public Vector3 currentVertexPosition;
+
+    public Vector3 currentVelocity;
+
+    public JellyVertex(int vertexIndex, Vector3 initialVertexPos, Vector3 currentVertexPos, Vector3 currentVel)
+    {
+        this.vertexIndex = vertexIndex;
+        this.intialVertexPosition = initialVertexPos;
+        this.currentVertexPosition = currentVertexPos;
+        this.currentVelocity = currentVel;
+    }
+
+    public Vector3 GetCurrentDisplacement()
+    {
+        return currentVertexPosition - intialVertexPosition;
+    }
+
+    public void UpdateVelocity(float bounceSpeed)
+    {
+        currentVelocity = currentVelocity - GetCurrentDisplacement() * bounceSpeed * Time.deltaTime;
+    }
+
+    public void Settle(float stiffness)
+    {
+        currentVelocity *= 1.0f - stiffness * Time.deltaTime;
+    }
+
+    public void ApplyPressureToVertex(Transform transform,Vector3 _position, float pressure)
+    {
+        Vector3 distanceVertexPoint = currentVertexPosition - transform.InverseTransformPoint(_position);
+        float adaptedPressure = pressure / (1f + distanceVertexPoint.sqrMagnitude*distanceVertexPoint.sqrMagnitude);
+        float velocity = adaptedPressure * Time.deltaTime;
+        currentVelocity += distanceVertexPoint.normalized * velocity;
+    }
+
+}
